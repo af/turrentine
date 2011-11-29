eeab492883f8b0d0b1e5056cb4b207177b8083c9
Aaron Franks <aaron.franks@gmail.com>
2011-11-29 10:02:23 -0800

    Fix copy/paste bugs and clarify docs in settings.py.

--- turrentine/settings.py
+++ turrentine/settings.py
@@ -7,13 +7,13 @@ template_dir_guess = getattr(settings, 'TEMPLATE_DIRS', ('templates',))[0]
 TURRENTINE_TEMPLATE_ROOT = getattr(settings, 'TURRENTINE_TEMPLATE_ROOT', template_dir_guess)
 
 # The directory where turrentine looks for templates that can be given as options
-# for CMS pages in the admin. Defaults to a relative location of '/cms/', but
-# you can override this to whatever you want:
+# for CMS pages in the admin. Defaults to a TURRENTINE_TEMPLATE_ROOT subdir named
+# 'cms', but you can override this to be whatever you want:
 default_setting = os.path.join(TURRENTINE_TEMPLATE_ROOT, 'cms')
-TURRENTINE_TEMPLATE_SUBDIR = getattr(settings, 'TURRENTINE_TEMPLATE_ROOT', default_setting)
+TURRENTINE_TEMPLATE_SUBDIR = getattr(settings, 'TURRENTINE_TEMPLATE_SUBDIR', default_setting)
 
 # The template to use for rendering a page if CMSPage.template_name is not specified:
-TURRENTINE_TEMPLATE_FALLBACK = getattr(settings, 'TURRENTINE_TEMPLATE_ROOT', 'turrentine/fallback.html')
+TURRENTINE_TEMPLATE_FALLBACK = getattr(settings, 'TURRENTINE_TEMPLATE_FALLBACK', 'turrentine/fallback.html')
 
 # List of MIME types to infer, based on the extension of the template name that was used:
 TURRENTINE_MIMETYPE_EXTENSIONS = getattr(settings, 'TURRENTINE_MIMETYPE_EXTENSIONS', (

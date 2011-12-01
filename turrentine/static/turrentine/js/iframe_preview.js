d8ac0e328aedfcec68b00de38b0cd3b8985725a3
Aaron Franks <aaron.franks@gmail.com>
2011-12-01 15:49:59 -0800

    Implement simple throttling for preview iframe refreshes.

--- turrentine/static/turrentine/js/iframe_preview.js
+++ turrentine/static/turrentine/js/iframe_preview.js
@@ -1,10 +1,10 @@
 (function(window, $) {
     var document = window.document;
-    var preview_iframe;
 
     var iframe_name = 'live_preview';   // Name (and id) to give to the preview iframe
     var preview_url = 'preview';        // Relative url to send the POST data to
-    var keypress_delay = 200;
+    var keypress_delay = 400;           // Number of milliseconds to wait before acting on keypress
+    var latest_keypress = new Date();
 
     $(document).ready(function() {
         var initial_url = $('.viewsitelink').attr('href');
@@ -14,18 +14,24 @@
 
         // Insert the iframe into the document:
         $('#id_content').after(preview);
-        preview_iframe = $('#live_preview');
 
         // Set the main form's target to the preview iframe, and POST to our
         // preview url to update the iframe's content.
-        // TODO: throttle updates
         $('#id_content').keyup(function(e) {
+            var timestamp = new Date();
+            latest_keypress = timestamp;
+
             setTimeout(function() {
-                form.attr('target', iframe_name)
-                    .attr('action', preview_url)
-                    .submit()
-                    .attr('target', '')     // Reset target and action after submitting
-                    .attr('action', '');
+                // If no keypresses have occurred since this one, refresh the
+                // iframe using the main form's data:
+                var another_key_was_pressed = (latest_keypress > timestamp);
+                if (!another_key_was_pressed) {
+                    form.attr('target', iframe_name)
+                        .attr('action', preview_url)
+                        .submit()
+                        .attr('target', '')     // Reset target and action after submitting
+                        .attr('action', '');
+                }
             }, keypress_delay);
         });
     })

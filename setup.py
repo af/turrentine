0a98d036430473b5d94b398d7d4d1bf23aabbbbd
Aaron Franks <aaron.franks@gmail.com>
2012-01-02 14:14:25 -0800

    Bump version number to 0.0.2.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup( name='turrentine',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'A very simple CMS for Django',
     url = 'https://github/af/turrentine',
 
